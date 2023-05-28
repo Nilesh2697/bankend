fe128abee57f0c56ec9888722935e86db073172f
Nilesh2697 <99721281+Nilesh2697@users.noreply.github.com>
2023-05-28 22:05:03 +0530

    backend

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,29 +1,6 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({
-
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
-    },
-
-    amount: {
-        type: Number,
-        require: true,
-    },
-
-    type: {
-        type: String,
-        enum: ['deposit', 'withdraw'],
-        required: true,
-    },
-
-    timestamp: {
-        type: Date,
-        default: Date.now,
-    },
-})
+const transactionSchema = 
 
 
 const Transaction = mongoose.model("transaction", transactionSchema);    
